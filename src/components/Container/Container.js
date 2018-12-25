fccd89f40fcf31d9f93f632e94d00ef7bfa03a09
Kamil Mirosz <kmirosz6@gmail.com>
2018-12-25 12:06:51 +0100

    create container style

--- src/components/Container/Container.js
+++ src/components/Container/Container.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 
-const Wrapper = styled.div`
-  margin: 0 auto;
-  max-width: 960px;
-  padding: 1.5rem 1rem;
-  ${({ isDebug }) => isDebug && 'background-color: orange;'}
-`;
+import { Wrapper } from './styled';
 
 const Container = ({ children, ...props }) => <Wrapper {...props}>{children}</Wrapper>;
 
